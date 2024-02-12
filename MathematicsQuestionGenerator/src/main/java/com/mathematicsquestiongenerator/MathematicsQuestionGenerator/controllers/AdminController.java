73cfaaedf0d3ab207f898c904b65b9fbb1dacb72
Mohamed Rilah <210049037@aston.ac.uk>
2024-02-12 17:17:10 +0000

    Added Admin Functionalities
    Allows Admin to either delete a Topic from the Topic table, or delete a User from the User table

--- MathematicsQuestionGenerator/src/main/java/com/mathematicsquestiongenerator/MathematicsQuestionGenerator/controllers/AdminController.java
+++ MathematicsQuestionGenerator/src/main/java/com/mathematicsquestiongenerator/MathematicsQuestionGenerator/controllers/AdminController.java
@@ -1,12 +1,14 @@
 package com.mathematicsquestiongenerator.MathematicsQuestionGenerator.controllers;
 
 import com.mathematicsquestiongenerator.MathematicsQuestionGenerator.model.Topics;
+import com.mathematicsquestiongenerator.MathematicsQuestionGenerator.model.User;
 import com.mathematicsquestiongenerator.MathematicsQuestionGenerator.repository.TopicsRepository;
 import com.mathematicsquestiongenerator.MathematicsQuestionGenerator.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -36,6 +38,26 @@ public class AdminController {
         topicsRepository.save(topics);
 
         return "admintopics";
+    }
+
+    @GetMapping({"/deleteTopic/{id}"})
+    public String deleteTopic(@PathVariable("id") int id) {
+        Topics topic = topicsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid id"));
+
+        topicsRepository.delete(topic);
+
+        return "redirect:/adminhomepage";
+    }
+
+    @GetMapping({"/deleteUser/{id}"})
+    public String deleteUser(@PathVariable("id") int id) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid id"));
+
+        userRepository.delete(user);
+
+        return "redirect:/adminhomepage";
     }
 
 }
